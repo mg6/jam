198dd5df7a27a9343e8291085922e7e192a0db63
Maciej Gamrat <dev@gamrat.it>
2025-02-27 18:58:02 +0100

    fix: logger name in entrypoint

--- jam/run.py
+++ jam/run.py
@@ -5,17 +5,17 @@ from jam.client import client
 
 logging.basicConfig(level=logging.DEBUG)
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(__package__)
 
 def run():
     try:
         with client:
-            logging.info("Jam time! Press Ctrl-C to quit.")
+            logger.info("Jam time! Press Ctrl-C to quit.")
             input()
     except (KeyboardInterrupt, EOFError):
         pass
 
-    logging.info("Bye!")
+    logger.info("Bye!")
     sys.exit(0)
 
 
